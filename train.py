ec0f5aaf613c09086b8c31769bd8af0ee7b1326d
wreckingralph06 <ralphwaldocandaza@gmail.com>
2024-03-17 14:50:13 +0800

    Created Neural Net model for calculating and reducing loss for each epoch iteration

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from model import NeuralNet
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -24,8 +25,6 @@ ignore_words = ['?', '!', '.', ',']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-print("all_words: ", all_words)
-print("tags: ", tags)
 
 x_train = []
 y_train = []
@@ -55,9 +54,45 @@ class ChatDataset(Dataset):
 
 #Hyperparameters
 batch_size = 8
+hidden_size = 8
+output_size = len(tags)
+input_size = len(x_train[0]) 
+learning_rate = 0.001
+num_epochs = 1000
+
+print("input_size: ", input_size)
+print("len(all_words): ", len(all_words))
+print("output size: ", output_size)
+print("tags: ", tags)
 
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
 
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#create model
+model = NeuralNet(input_size, hidden_size, output_size)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device).long()
+
+        #forward pass
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+
+        #backward and optimizer step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch + 1) % 100 == 0:
+        print(f'epoch {epoch+1}/{num_epochs}, loss={loss.item():.4f}')
+
+print(f'final loss, loss={loss.item():.4f}')
 
 
